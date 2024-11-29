997afe8105188da6b0678a372747e9d3f6434b88
vanesamrd <paneja435@gmail.com>
2024-11-29 20:19:24 +0700

    before modif

--- Percobaan2.java
+++ Percobaan2.java
@@ -3,23 +3,20 @@ public class Percobaan2 {
     
     static int hitungPangkat(int x, int y) {
         if (y == 0) {
-            System.out.print("1");
             return (1);
         } else {
-            System.out.print(x + "x");
             return (x * hitungPangkat(x, y - 1));
         }
     }
         public static void main(String[] args) {
             Scanner sc = new Scanner(System.in);
     
-            System.out.print("Bilangan yang dihitung : ");
+            System.out.println("Bilangan yang dihitung: ");
             int bilangan = sc.nextInt();
-            System.out.print("Pangkat : ");
+            System.out.println("Pangkat: ");
             int pangkat = sc.nextInt();
-    
-            System.out.print("Deret hitungan : ");
-            int hasil = hitungPangkat(bilangan, pangkat);
-            System.out.print(" = " + hasil);
+
+            System.out.println(hitungPangkat(bilangan, pangkat));
         }
+        
 }
